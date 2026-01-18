2721c308ef856b18ce9caff4ee33bcfe4d193806
duongdai184 <daiduongnguyendac@gmail.com>
2026-01-19 00:00:04 +0700

    demopart01

---
+++ Demopart1/midTermExam/Assets/Blinking.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Blinking : MonoBehaviour
+
+{
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        spriteRenderer.enabled = !spriteRenderer.enabled;
+    }
+}
---
+++ Demopart1/midTermExam/Assets/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+
+        var worldPoint =
+        Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPoint.z = 0;
+        transform.position = worldPoint;
+    }
+}
